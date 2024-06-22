d77435ba020460fb341766b6eed8c6be9e25e79e
Hackfront <hackfront@protonmail.ch>
2024-06-22 16:08:04 +0200

    Add logging

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
-import test_tss
+import logging
+
+from config import log_level
+
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(name)s: %(message)s', level=log_level)
+logger = logging.getLogger(__name__)
+logger.info(f'Process started')
+
+import test_consensus
