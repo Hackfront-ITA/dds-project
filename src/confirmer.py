4f26418505b44047b69707fe023543d974f282d2
Hackfront <hackfront@protonmail.ch>
2024-06-24 12:18:16 +0200

    Small fixes in confirmer

--- src/confirmer.py
+++ src/confirmer.py
@@ -47,11 +47,14 @@ def on_submit_l(instance, value):
 
     share = tss_share_sign()
     id = instance.id.hex()
-    sig = None
+    sig = x_sign([ 'submit', value, share ])
 
     beb.trigger('send', [ id, 'submit', value, share, sig ])
 
 def on_submit_r(instance, sender, value, share, sig):
+    if not x_verify([ 'submit', value, share ]):
+        return
+
     if not tss_share_verify(process, value, share):
         return
 
@@ -63,27 +66,28 @@ def on_submit_r(instance, sender, value, share, sig):
 
     instance.xfrom.append(process)
     if len(instance.xfrom) >= num_hosts - max_failures:
-        instance.trigger('from-over-th')
+        instance.trigger('e_from_over_th')
 
     instance.light_cert.append(share)
-    instance.full_cert.append([ 'submit', value, share, msig ])
+    instance.full_cert.append([ 'submit', value, share, sig ])
 
 def on_from_over_th(instance):
     instance.confirmed = True
     instance.trigger('confirm', value)
 
     combined = tss_combine(instance.light_cert)
-    beb.trigger('send', [ 'light-cert', instance.value, combined ])
+    beb.trigger('send', [ 'light-certificate', instance.value, combined ])
 
 def on_light_certificate(instance, sender, value, light_cert):
     if not valid(light_cert, value):
         return
 
     instance.obt_light_certs.append([ 'light-certificate', value, light_cert ])
-    if False:
-        instance.trigger('conflicting-light-certs', cert_1, cert_2)
+    if False and instance.confirmed:
+        instance.trigger('e_conflicting_full_certs', cert_1, cert_2)
 
 def on_conflicting_light_certs(instance, cert_1, cert_2):
+    beb.trigger('send', [ 'full-certificate', instance.value, instance.full_cert ])
     pass
 
 def on_full_certificate(instance, sender, value, full_cert):
@@ -95,7 +99,17 @@ def on_full_certificate(instance, sender, value, full_cert):
         instance.trigger('conflicting-full-certs', cert_1, cert_2)
 
 def on_conflicting_full_certs(instance, cert_1, cert_2):
-    pass
+    proof = []
+    for i in range(num_hosts):
+        if True:
+            continue
+
+        pair = (m1, m2)
+        proof.append(pair)
+
+    f = get_processes(proof)
+    
+    instance.trigger('detect', f, proof)
 
 
 def on_receive(_, source, payload):
