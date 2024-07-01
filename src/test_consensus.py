9f077d5593d87ffd4407be90c1bf44669958a245
Hackfront <hackfront@protonmail.ch>
2024-07-01 16:06:09 +0200

    Use ABC as consensus

--- src/test_consensus.py
+++ src/test_consensus.py
@@ -2,15 +2,22 @@ from logging import getLogger
 from random import randint
 from time import sleep
 
+from dds_abc import ABC
+from config import t0_ac
 from consensus import FloodingConsensus
 
 logger = getLogger(__name__)
-consensus = FloodingConsensus()
+consensus = ABC(t0_ac)
 
 def on_decision(_, decision):
-    logger.info(f'Consensus decided {decision}')
+	logger.info(f'Consensus decided {decision}')
 
 consensus.on('decide', on_decision)
+
+def on_detect(_, processes, proof):
+	logger.info(f'ABC detected {processes} with {proof}')
+
+consensus.on('detect', on_detect)
 
 sleep(1)
 
